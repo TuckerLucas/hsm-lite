cf9c95a1b2d140ac920c622d3100e40257404bb0
lucas <lucas.tucker.dev@gmail.com>
2025-08-16 12:31:32 +0100

    Add first tests

--- keystore/keystore.cpp
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello from HSM lite\n";
-}
---
+++ keystore/src/keystore.cpp
@@ -0,0 +1,5 @@
+#include "keystore.hpp"
+
+int add(int a, int b) {
+    return a + b;
+}
