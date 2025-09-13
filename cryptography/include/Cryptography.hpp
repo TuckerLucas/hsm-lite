a035f9f970de864faa7463ef266d15acc38fd69b
lucas <lucas.tucker.dev@gmail.com>
2025-09-13 18:38:21 +0100

    Implement initial hash key functionality
    Also restructured common header files to core folder.

--- cryptography/include/Cryptography.hpp
+++ cryptography/include/Cryptography.hpp
@@ -1,6 +1,12 @@
 #pragma once
 
+#include "Key.hpp"
+#include "KeystoreStatus.hpp"
+
+using Hash256 = std::array<uint8_t, 32>;
+
 class Cryptography
 {
-
+public: 
+    Hash256 hashKeySha256(KeyData keyData);
 };
